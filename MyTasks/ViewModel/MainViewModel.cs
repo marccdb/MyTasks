b8fa1920928a135a07998c2337eedcbcfa3c718c
Marcio Del Bianco <marcio.bianco@gmail.com>
2022-10-10 15:07:16 -0300

    add MainViewModel

---
+++ MyTasks/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MyTasks.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        string newTask;
+
+
+    }
+}
