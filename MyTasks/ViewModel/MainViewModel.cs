126f3994ff1efb5c0b6973681054e67c4d86e881
Marcio Del Bianco <marcio.bianco@gmail.com>
2022-10-10 16:00:40 -0300

    add string bindings

--- MyTasks/ViewModel/MainViewModel.cs
+++ MyTasks/ViewModel/MainViewModel.cs
@@ -1,12 +1,30 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
 
 namespace MyTasks.ViewModel
 {
     public partial class MainViewModel : ObservableObject
     {
+        public MainViewModel()
+        {
+            Items = new ObservableCollection<string>();
+        }
+
+        [ObservableProperty]
+        ObservableCollection<string> items;
+
         [ObservableProperty]
         string newTask;
 
+        [RelayCommand]
+        void AddTask()
+        {
+            if (string.IsNullOrWhiteSpace(NewTask)) return;
+
+            Items.Add(NewTask);
+            NewTask = string.Empty;
+        }
 
     }
 }
